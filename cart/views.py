f683154d1e7f56a1c52888d2e83ee66e327cccec
dvcoffey <dvcoffey@gmail.com>
2021-04-23 12:09:29 +0000

    Adjust cart view added to cart/views.py. Implemented on cart.html to for ability to adjust the amount of items in the cart.

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -35,3 +35,29 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the items in the cart"""
+
+    quantity = int(request.POST.get('quantity'))
+    gauge = None
+    if 'string_gauge' in request.POST:
+        gauge = request.POST['string_gauge']
+    cart = request.session.get('cart', {})
+
+    if gauge:
+        if quantity > 0:
+            cart[item_id]['items_by_gauge'][gauge] = quantity
+        else:
+            del cart[item_id]['items_by_size'][gauge]
+            if not cart[item_id]['items_by_gauge']:
+                cart.pop(item_id)
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
