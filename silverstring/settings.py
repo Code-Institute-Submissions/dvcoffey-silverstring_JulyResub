c577594d73fd70edc1099d7920b99caf2adf97c5
dvcoffey <dvcoffey@gmail.com>
2021-04-24 03:53:19 +0000

    media processor added to settings.py

--- silverstring/settings.py
+++ silverstring/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +60,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'silverstring.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
