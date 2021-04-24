9899b51c308fd7f5a8539e16f2952f285f19fd7d
dvcoffey <dvcoffey@gmail.com>
2021-04-24 04:47:39 +0000

    Added stripe_elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IjcrSJsYfaQBI6AjpjM2aGUm3sgMNnBnTihsycuCDrRkNUHBGtMdnfroZxoWSYOQpZU7HrvlrV19LSfF9iqLtoc00A3qv1tdu',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
