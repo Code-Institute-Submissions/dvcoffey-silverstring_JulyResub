c5b860aa313779a8b5f8ba7681ab324054788388
dvcoffey <dvcoffey@gmail.com>
2021-04-29 13:04:37 +0000

    removed secret key and set debug

--- silverstring/settings.py
+++ silverstring/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0m2zx_$2ph!xl0rqetj5nt-t0tox*jo30jgh8#1w&p*(43gf@v'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['silverstring.herokuapp.com', 'localhost']
 
