d57cdde60bc95868b8e0568e2ae570a8aa1148e8
dvcoffey <dvcoffey@gmail.com>
2021-04-30 08:44:25 +0000

    reviews altered

--- products/models.py
+++ products/models.py
@@ -26,6 +26,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    review = models.TextField()
 
     def __str__(self):
         return self.name
