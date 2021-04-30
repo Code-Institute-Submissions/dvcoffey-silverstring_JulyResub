98c9a0a6d52fab2cdf171c6068211553dd8c55fa
dvcoffey <dvcoffey@gmail.com>
2021-04-30 09:43:14 +0000

    reviews removed

--- products/views.py
+++ products/views.py
@@ -60,11 +60,9 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
-    reviews = Review.objects.filter(product=product)
 
     context = {
         'product': product,
-        'reviews': reviews,
     }
 
     return render(request, 'products/product_detail.html', context)
