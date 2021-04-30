d18e44b28d8282a2c6bfa019753425a46e5d18e0
dvcoffey <dvcoffey@gmail.com>
2021-04-30 05:20:21 +0000

    reviews altered

--- products/models.py
+++ products/models.py
@@ -34,11 +34,11 @@ class Product(models.Model):
 class Review(models.Model):
     product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
-
+    name = 'review'
     content = models.TextField(blank=True, null=True)
     stars = models.IntegerField()
 
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.user
+        return self.name
