263db45fa8fa1c4e645a1c573667045db2653949
dvcoffey <dvcoffey@gmail.com>
2021-07-01 14:44:22 +0000

    set debug to true

--- silverstring/settings.py
+++ silverstring/settings.py
@@ -27,7 +27,7 @@ DISABLE_COLLECTSTATIC = 0
 SECRET_KEY = os.environ.get('SECRET_KEY', 'none')
 
 
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['silverstring.herokuapp.com', 'localhost']
@@ -192,7 +192,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.pip3 '
 
     # static & media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
