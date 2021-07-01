fe0757c2a556d0cda7f300293f246f70604302e1
dvcoffey <dvcoffey@gmail.com>
2021-07-01 15:03:27 +0000

    undo previous commit

--- silverstring/settings.py
+++ silverstring/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DISABLE_COLLECTSTATIC = 1
+DISABLE_COLLECTSTATIC = 0
 SECRET_KEY = os.environ.get('SECRET_KEY', 'none')
 
 
